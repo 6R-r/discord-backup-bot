578f8a82186f8143fa7b1f543c085be6a1b17e9e
emirhanbaltas34 <77491112+emirhanbaltas34@users.noreply.github.com>
2021-07-27 18:45:07 +0300

    Add files via upload

---
+++ events/clickButton.js
@@ -0,0 +1,20 @@
+const Embed = require("../utils/Embed");
+const db = require("../utils/database");
+
+module.exports = (client, button) => {
+    if (button.id === "tr") {
+        button.channel.send(
+            new Embed(
+                `<@${button.clicker.id}> **Türkçe** dili başarıyla seçildi :white_check_mark:`
+            )
+        );
+        db.set(`lang_${button.clicker.id}`, "tr");
+    } else if (button.id === "en") {
+        button.channel.send(
+            new Embed(
+                `<@${button.clicker.id}> **English** language has been selected successfully :white_check_mark:`
+            )
+        );
+        db.set(`lang_${button.clicker.id}`, "en");
+    }
+};
