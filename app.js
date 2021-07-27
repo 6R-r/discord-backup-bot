a340825b6544a85b3036c070da2a19d099e0e845
emirhanbaltas34 <77491112+emirhanbaltas34@users.noreply.github.com>
2021-07-27 18:45:22 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+require("discord-buttons")(client);
+
 client.commands = new Discord.Collection();
 
 new (require(`./utils/commandHandler`))(client);
